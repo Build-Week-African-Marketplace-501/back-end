77f1aef8d7a02f03c11c2bdd467f5ff4e4b95da3
June Mcmillan <mcmillanj0106@yahoo.com>
2022-01-21 13:10:15 -0500

    Update auth-router.js

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,34 +1,52 @@
-/* eslint-disable */
-
 const router = require('express').Router()
 
-// const Users = require('../users/user-model')
-const { validator } = require('../global-middleware')
+const { validator, sendUnpackedEntry, sendMessage } = require('../global-middleware')
 const { userSchema } = require('../users/user-schema')
-const { usernameIsUnique, userIdExists } = require('../users/user-middleware')
-const { hashPassword, authenticate, constructToken } = require('./auth-middleware')
+const { getUser, usernameIsUnique, userExists, registerUser } = require('../users/user-middleware')
+const {
+  restricted,
+  hashPassword,
+  authenticate,
+  constructToken,
+  constructPayload,
+} = require('./auth-middleware')
 
 // [POST] /api/auth/register
 router.post(
   '/register',
-  [validator(userSchema), usernameIsUnique, hashPassword],
-  (req, res, next) => {
-    res.status(201).json({ message: '[POST] /api/auth/register' })
-  }
+  [validator(userSchema), getUser, usernameIsUnique, hashPassword, registerUser],
+  sendMessage('user registered successfully')
 )
 
 // [POST] /api/auth/login
 router.post(
   '/login',
-  [validator(userSchema), userIdExists, authenticate, constructToken],
-  (req, res, next) => {
-    res.json({ message: '[POST] /api/auth/login', data: {} })
-  }
+  [validator(userSchema), getUser, userExists, authenticate, constructToken, constructPayload],
+  sendUnpackedEntry('user')
 )
 
 // [GET] /api/auth/logout
-router.get('/logout', (req, res, next) => {
-  res.json({ message: '[GET] /api/auth/logout' })
-})
+router.get('/logout', restricted, sendMessage('user logged out successfully'))
 
 module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
